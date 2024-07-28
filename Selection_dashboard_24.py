1809c20961d21836a801799632bb79a2c4ef077d
Jake Farren-Price <57045388+Jakefp@users.noreply.github.com>
2024-07-28 11:31:04 +0200

    Update Selection_dashboard_24.py password added

--- Selection_dashboard_24.py
+++ Selection_dashboard_24.py
@@ -1,7 +1,35 @@
+import hmac
 import streamlit as st
-
 import pandas as pd
 import openpyxl
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
 
 file_path = 'CoachSailorReviewFormSelection_Cognito export.xlsx'
 excel_data = pd.read_excel(file_path, engine='openpyxl')
