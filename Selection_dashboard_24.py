11105ae1c7e5accc9a8ea145eb77bd06e3bf8cf5
Jake Farren-Price <57045388+Jakefp@users.noreply.github.com>
2024-07-28 10:53:56 +0200

    Update Selection_dashboard_24.py updated file location

--- Selection_dashboard_24.py
+++ Selection_dashboard_24.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import openpyxl
 
-file_path = 'old_examples/CoachSailorReviewFormSelection_Cognito export.xlsx'
+file_path = 'CoachSailorReviewFormSelection_Cognito export.xlsx'
 excel_data = pd.read_excel(file_path, engine='openpyxl')
 
 # Title of the app
